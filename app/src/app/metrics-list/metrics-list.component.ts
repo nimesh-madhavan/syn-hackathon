95d82eabbf5c93ad19fe9fa9a506de413227a6df
abajpaye <abajpaye@cisco.com>
2019-06-20 14:20:25 +0530

    metrics-list component

---
+++ app/src/app/metrics-list/metrics-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-metrics-list',
+  templateUrl: './metrics-list.component.html',
+  styleUrls: ['./metrics-list.component.css']
+})
+export class MetricsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
