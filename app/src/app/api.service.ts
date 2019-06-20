85de6296f73d1050608c9e615f1352e83c31dce9
nimmadha <nmadhavan@synamedia.com>
2019-06-20 16:22:57 +0530

    Api service

--- app/src/app/api.service.ts
+++ app/src/app/api.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+
+const apiServer = "http://localhost:3000";
+const jobsApi = "/api/jobs/";
+const metricsApiSuffix = "/metadata"
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getJobs(server: string): Observable<string[]> {
+    var url = apiServer + jobsApi + server;
+    return this.http.get<string[]>(url);
+  }
+
+  getMetrics(server: string, job: string): Observable<string[]> {
+    var url = apiServer + jobsApi + server + "/" + job + metricsApiSuffix;
+    return this.http.get<any[]>(url);
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerListComponent } from './server-list/server-list.component';
 import {MetricsListComponent}  from './metrics-list/metrics-list.component'
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {MetricsListComponent}  from './metrics-list/metrics-list.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
