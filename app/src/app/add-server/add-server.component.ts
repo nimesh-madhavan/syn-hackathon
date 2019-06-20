fe9a51be4eb1e7177dd9c5c6263af4c3492b2c29
rajivr ranjan <rajivr2@cisco.com>
2019-06-21 01:29:27 +0530

    Show error while adding invalid server

--- app/src/app/add-server/add-server.component.ts
+++ app/src/app/add-server/add-server.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStoreService } from '../local-store.service'
+import { ApiService } from '../api.service'
+//import $ from "jquery";
 
 @Component({
   selector: 'app-add-server',
@@ -9,8 +11,9 @@ import { LocalStoreService } from '../local-store.service'
 export class AddServerComponent implements OnInit {
   serverName = ""
   serverUrl = ""
-  alertMessageEmpty= "Name or URL Field is Empty !!"
-  constructor(private localStore: LocalStoreService) {
+  alertMessageEmpty = "Name or URL Field is Empty !!"
+  invalidServer = "";
+  constructor(private localStore: LocalStoreService, private apiService: ApiService) {
   }
 
   ngOnInit() {
@@ -18,14 +21,28 @@ export class AddServerComponent implements OnInit {
   }
 
   AddServers() {
-    if (this.serverName == "" || this.serverUrl==""){
+    if (this.serverName == "" || this.serverUrl == "") {
       alert(this.alertMessageEmpty);
       return;
     }
+    this.apiService.getJobs(this.serverUrl).subscribe(item => {
+      if (item.length == 0) {
+        this.invalidServer = "Invalid server"
+      }
+      else {
+        this.invalidServer = "";
+        this.localStore.AddServers(this.serverName, this.serverUrl);
+        this.serverName = "";
+        this.serverUrl = "";
+        var abc = document.getElementById("serverModal");
+        console.log(abc);
+        window.location.reload();
+      }
+    });
 
-    this.localStore.AddServers(this.serverName, this.serverUrl);
-    this.serverName = "";
-    this.serverUrl = "";
-    window.location.reload();
+    //this.localStore.AddServers(this.serverName, this.serverUrl);
+    //this.serverName = "";
+    //this.serverUrl = "";
+    //window.location.reload();
   }
 }
